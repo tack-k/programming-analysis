321e294bcbc5a2bcf537ee468965d29b62183ed3
tack-k <tyool9c@gmail.com>
2020-09-24 21:24:27 +0900

    質問ページの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root to: 'studies#index'
-  resources :studies, only: [:index]
+  resources :studies, only: [:index] do
+  collection do
+    get 'analysis'
+  end
+end
 end
