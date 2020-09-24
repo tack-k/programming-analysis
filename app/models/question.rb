85b8c50c0a18a237a436d5904e4c84ef07410ac3
tack-k <tyool9c@gmail.com>
2020-09-24 15:07:30 +0900

    studiesコントローラー作成

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,6 +2,6 @@ class Question < ActiveHash::Base
   self.data = [
     { id: 1, No: 'Q.1', question: 'HTML,CSSが何なのかわかる' },
     { id: 2, No: 'Q.2', question: 'プログラミングを行う目的は仕事にしたいからだ' },
-    { id: 3, No: 'Q.3', question: '得られる効果が高ければ、資金の投資はいとわない' },
+    { id: 3, No: 'Q.3', question: '得られる効果が高ければ、資金の投資はいとわない' }
   ]
 end
