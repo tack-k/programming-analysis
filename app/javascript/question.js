f6dec61cb2c5a7a07f7041d1453052862ed607ac
tack-k <tyool9c@gmail.com>
2020-09-27 11:32:20 +0900

    微修正

--- app/javascript/question.js
+++ app/javascript/question.js
@@ -1,11 +1,10 @@
 $(function() {
- 
-  $(".button").on("click", function() {
 
+  $(".button").on("click", function() {
     $(this).closest("div").css("display", "none")
     id = $(this).attr("href");
     $(id).show("fast")
     return false;
    });
   
-  })
+  });
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'studies#index'
-  resources :studies, only: [:index] do
+  resources :studies, only: [:index, :new, :create, :show] do
   collection do
     get 'analysis'
   end
