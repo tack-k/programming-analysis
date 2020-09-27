ee86e4b79808fbb9ec93a4c8328104dbe57abfb2
tack-k <tyool9c@gmail.com>
2020-09-27 11:31:41 +0900

    データ作成にかかるアクションの設定

--- app/controllers/studies_controller.rb
+++ app/controllers/studies_controller.rb
@@ -4,9 +4,33 @@ class StudiesController < ApplicationController
 
   end
 
+  def new
+    @study = Study.new
+    @studys = Study.all
+  end
+
+  def create
+    @study = Study.new(studies_params)
+    if @study.save
+      redirect_to new_study_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @study = Study.find(params[:id])
+  end
+
   def analysis
     @analysis1 = Analysis.find(1)
     @analysis2 = Analysis.find(2)
     @analysis3 = Analysis.find(3)
   end
+
+  private
+  def studies_params
+    params.require(:study).permit(:method, :content, :reason, :price, :image)
+  end
+
 end
