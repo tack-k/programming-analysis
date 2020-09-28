35bf1f660b6cc6caca2facd2c4b330db0b1cb11e
tack-k <tyool9c@gmail.com>
2020-09-28 11:18:12 +0900

    バリデーション、エラーメッセージの表示

--- app/models/study.rb
+++ app/models/study.rb
@@ -3,4 +3,12 @@ class Study < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :analysis, shortcut: [:no, :question]
+
+  with_options presence: true do
+    validates :image
+    validates :method
+    validates :content
+    validates :reason
+    validates :price
+  end
 end
