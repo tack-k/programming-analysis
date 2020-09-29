dd8e89870ab1f3f5d2c6db085d3db20ad0d0cade
tack-k <tyool9c@gmail.com>
2020-09-29 19:50:30 +0900

    resultアクティブハッシュ作成

---
+++ app/models/result.rb
@@ -0,0 +1,10 @@
+class Result < ActiveHash::Base
+  self.data = [
+    { id: '', title: '---' },
+    { id: 1, title: 'プロゲート' },
+    { id: 2, title: 'デイトラ' },
+    { id: 3, title: 'テックキャンプ-エンジニア転職-' },
+    { id: 4, title: 'テックキャンプ-プログラミング教養-' }
+  ]
+end
+
