8291b9a76f19ae3760472eb9e932ad078aefe07d
tack-k <tyool9c@gmail.com>
2020-09-29 21:29:48 +0900

    deviseストロングパラメーター設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+
+  before_action :confifure_permitted_parameters, if: :devise_controller?
+
+private
+def confifure_permitted_parameters
+  devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :result, :admin]) 
+end
+
 end
