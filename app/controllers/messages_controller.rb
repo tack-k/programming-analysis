7b5a99478cb2c02eb26f62cacc6ead0eddcfd7c9
tack-k <tyool9c@gmail.com>
2020-10-04 11:39:09 +0900

    room,messageコントローラーのアクション設定(仮)

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,11 +1,19 @@
 class MessagesController < ApplicationController
 
   def index
-
+    @users = User.all
+    @messages = Message.all
+    @message = Message.new
+    @room = Room.find(params[:room_id])
   end
 
   def create
-    
+    @message = Message.new(message_params)
   end
 
+
+  private
+  def message_params
+    params.require(:message).permit(:message, :admin_id, :user_id, :room_id)
+  end
 end
--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,10 +1,23 @@
 class RoomsController < ApplicationController
 
   def new
-
+    @users = User.all
+    @room = Room.new
   end
 
   def create
-    
+    @room = Room.new(room_params)
+    if @room.save
+      redirect_to "/rooms/#{@room.id}/messages"
+    else
+      render :new
+    end
   end
+
+  private
+
+  def room_params
+    params.require(:room).permit(:user_id)
+  end
+
 end
