91eafac9e7ea948f3f2f7a915e0798a599066c50
tack-k <tyool9c@gmail.com>
2020-10-08 22:17:44 +0900

    チャットルーム作成(user作成時運営者をルームに入れる)

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,12 +1,28 @@
 class RoomsController < ApplicationController
 
+  def index
+    @rooms = Room.includes(:messages).order("messages.created_at") #orderで他テーブル使う場合はjoinsで内部結合する
+    @users = User.all
+  end
+
   def new
-    @user = current_user
-    @room = @user.room
+    @room = Room.new
+    @admins = Admin.all
   end
 
   def create
-
+    @room = Room.new(room_params)
+    if @room.save
+      redirect_to room_messages_path(@room.id)
+    else
+      redirect_to new_room_path
+    end
   end
+
+private
+
+def room_params
+  params.require(:room).permit(admin_ids: []).merge(user_id: current_user.id)
+end
 
 end
