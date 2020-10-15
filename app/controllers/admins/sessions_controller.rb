5d82f1c282932f6dec28540b2fe7ed0d2eed4c50
tack-k <tyool9c@gmail.com>
2020-10-15 11:25:34 +0900

    管理者ログアウト・リダイレクト先の変更

--- app/controllers/admins/sessions_controller.rb
+++ app/controllers/admins/sessions_controller.rb
@@ -24,6 +24,11 @@ class Admins::SessionsController < Devise::SessionsController
     admins_index_path(resource)
   end
 
+  def after_sign_out_path_for(resource)
+    new_admin_session_path
+  end 
+
+
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
