7d4f5fdfafdfca418c2ecf06e6d4b840de65fc26
tack-k <tyool9c@gmail.com>
2020-10-17 10:23:12 +0900

    リダイレクト微修正

--- app/controllers/admins/sessions_controller.rb
+++ app/controllers/admins/sessions_controller.rb
@@ -21,7 +21,7 @@ class Admins::SessionsController < Devise::SessionsController
   protected
 
   def after_sign_in_path_for(resource)
-    admins_index_path(resource)
+    admins_index_path
   end
 
   def after_sign_out_path_for(resource)
