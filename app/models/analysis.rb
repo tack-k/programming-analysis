8320a34978978aa128e6f89f9a85b832d5e6f7c1
tack-k <tyool9c@gmail.com>
2020-10-17 10:16:50 +0900

    質問内容の変更

--- app/models/analysis.rb
+++ app/models/analysis.rb
@@ -1,8 +1,8 @@
 
 class Analysis < ActiveHash::Base
   self.data = [
-    { id: 1, no: 'Q.1', question: 'HTML,CSSの意味か説明できる' },
-    { id: 2, no: 'Q.2', question: 'プログラミングを行う目的は仕事にしたいからだ' },
-    { id: 3, no: 'Q.3', question: '得られる効果が高ければ、資金の投資はいとわない' }
+    { id: 1, no: 'Q.1', question: 'HTML,CSSの役割を説明できる' },
+    { id: 2, no: 'Q.2', question: 'プログラミングを仕事にしたい' },
+    { id: 3, no: 'Q.3', question: '得られる効果が高ければ、投資はいとわない' }
   ]
 end
