5466b109614e30774a332997c312c7d3122eede1
tack-k <tyool9c@gmail.com>
2020-10-20 18:11:46 +0900

    外部キーの同時削除設定

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,7 +2,7 @@ class Room < ApplicationRecord
 
   has_many :room_admins
   has_many :admins, through: :room_admins
-  belongs_to :users, optional: true
+  belongs_to :users
   has_many :messages
 
   validates :user_id, uniqueness: true
