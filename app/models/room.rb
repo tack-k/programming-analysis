bb8a75e55243352d4a993eb636cafbbf3c762760
tack-k <tyool9c@gmail.com>
2020-10-20 19:24:30 +0900

    微修正

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,7 +2,7 @@ class Room < ApplicationRecord
 
   has_many :room_admins
   has_many :admins, through: :room_admins
-  belongs_to :users
+  belongs_to :users, optional: true
   has_many :messages
 
   validates :user_id, uniqueness: true
