56f3bda3e85a47dfe7eddd2035cb220f12025662
tack-k <tyool9c@gmail.com>
2020-10-21 11:17:36 +0900

    診断結果にUdemy追加

--- app/models/result.rb
+++ app/models/result.rb
@@ -4,7 +4,8 @@ class Result < ActiveHash::Base
     { id: 1, title: 'プロゲート' },
     { id: 2, title: 'デイトラ' },
     { id: 3, title: 'テックキャンプ-エンジニア転職-' },
-    { id: 4, title: 'テックキャンプ-プログラミング教養-' }
+    { id: 4, title: 'テックキャンプ-プログラミング教養-' },
+    { id: 5, title: 'Udemy' }
   ]
 end
 
