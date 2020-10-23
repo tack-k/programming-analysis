b61f75ae0039a74f933737e97098c0a4587af44b
tack-k <tyool9c@gmail.com>
2020-10-23 21:31:25 +0900

    roomモデル単体テストコード

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,7 +1,7 @@
 class Room < ApplicationRecord
   has_many :room_admins
   has_many :admins, through: :room_admins
-  belongs_to :users, optional: true
+  belongs_to :user, optional: true
   has_many :messages
 
   validates :user_id, uniqueness: true
