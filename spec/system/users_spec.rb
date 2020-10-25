65b9c781686343eb7ad808626c7575267a065be6
tack-k <tyool9c@gmail.com>
2020-10-25 15:46:39 +0900

    ユーザーログイン機能

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -51,3 +51,68 @@ RSpec.describe "ユーザー新規登録", type: :system do
   end
 end
 
+RSpec.describe "ログイン", type: :system do
+  before do
+    @user = FactoryBot.create(:user)
+  end
+  
+  context 'ログインができる場合' do
+    it '保存されたユーザー情報と一致すればログインができ、@roomが作成されていればroom#loginに遷移する' do
+      @room = FactoryBot.create(:room, user_id: @user.id)
+      #トップページに移動する
+      visit root_path 
+      #[チャット相談]というリンクがある
+      expect(page).to have_link 'チャット相談'
+      #[チャット相談]をクリックする
+      click_on 'チャット相談'
+      #user#sign_inに遷移している
+      expect(current_path).to eq  new_user_session_path
+      #正しいメールアドレス、パスワードを入力する
+      fill_in 'メールアドレス', with: @user.email
+      fill_in 'パスワード', with: @user.password
+      #ログインボタンを押す
+      find('input[name="commit"]').click
+      #@roomが作成されていればroom#loginに遷移している
+      expect(current_path).to eq login_rooms_path
+    end
+
+    it '保存されたユーザー情報と一致すればログインができ、@roomが作成されていなければroom#newに遷移する' do
+      #トップページに移動する
+      visit root_path 
+      #[チャット相談]というリンクがある
+      expect(page).to have_link 'チャット相談'
+      #[チャット相談]をクリックする
+      click_on 'チャット相談'
+      #user#sign_inに遷移している
+      expect(current_path).to eq  new_user_session_path
+      #正しいメールアドレス、パスワードを入力する
+      fill_in 'メールアドレス', with: @user.email
+      fill_in 'パスワード', with: @user.password
+      #ログインボタンを押す
+      find('input[name="commit"]').click
+      #@roomが作成されていなけれroom#newに遷移している
+      expect(current_path).to eq new_room_path
+    end
+  end
+
+  context 'ログインができない場合' do
+    it '保存されたユーザー情報と一致しないときは、user#sign_inに戻される' do
+      @room = FactoryBot.create(:room, user_id: @user.id)
+      #トップページに移動する
+      visit root_path 
+      #[チャット相談]というリンクがある
+      expect(page).to have_link 'チャット相談'
+      #[チャット相談]をクリックする
+      click_on 'チャット相談'
+      #user#sign_inに遷移している
+      expect(current_path).to eq new_user_session_path
+      #誤ったメールアドレス、パスワードを入力する
+      fill_in 'メールアドレス', with: ''
+      fill_in 'パスワード', with: ''
+      #ログインボタンを押す
+      find('input[name="commit"]').click
+      #user#sign_inに戻される
+      expect(current_path).to eq new_user_session_path
+    end
+  end
+end
