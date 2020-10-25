c436f6ef89da2d5fd4a1419e3d45da95764ecf32
tack-k <tyool9c@gmail.com>
2020-10-25 15:46:05 +0900

    新規ユーザー登録

--- spec/factories/studies.rb
+++ spec/factories/studies.rb
@@ -2,9 +2,9 @@ FactoryBot.define do
   factory :study do
     content { Faker::Lorem.paragraphs }
     reason { Faker::Lorem.paragraphs }
-    price { Faker::Lorem.words }
+    price { Faker::Lorem.word }
     url { Faker::Lorem.characters }
-    # method {Faker::Lorem.words}
+    # method { Faker::Lorem.word }
 
     after(:build) do |study|
       study.image.attach(io: File.open('public/images/test-image.png'), filename: 'test-image.png')
--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,9 +1,53 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :system do
+RSpec.describe "ユーザー新規登録", type: :system do
   before do
-    driven_by(:rack_test)
+    @user = FactoryBot.build(:user)
+    @study = FactoryBot.create(:study, method: 'プロゲート')
   end
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+  context '新規登録ができる場合' do
+    it '正しい情報を入力すれば新規登録でき、study#showに遷移する' do
+      #study#showに移動する
+      visit study_path(@study.id) 
+      #study#showページに[無料相談する]ボタンがある
+      expect(page).to have_link '無料相談する'
+      #[無料相談する]ボタンをクリックする
+      click_on '無料相談する'
+      #新規登録ページへ遷移している
+      expect(current_path).to eq new_user_registration_path
+      #ユーザー情報を入力する
+      fill_in 'ユーザーネーム', with: @user.username
+      fill_in 'メールアドレス', with: @user.email
+      select 'プロゲート', from: "result"
+      fill_in 'パスワード', with: @user.password
+      fill_in 'パスワード(確認)', with: @user.password_confirmation
+      #会員登録ボタンを押すとユーザーモデルカウントが1上がる
+      expect{find('input[name="commit"]').click}.to change {User.count}.by(1)
+      #room#newページへ遷移している
+      expect(current_path).to eq new_room_path
+    end
+
+    it '正しくない情報を入力すると登録できず、user#newに戻される' do
+      #study#showに移動する
+      visit study_path(@study.id) 
+      #study#showページに[無料相談する]ボタンがある
+      expect(page).to have_link '無料相談する'
+      #[無料相談する]ボタンをクリックする
+      click_on '無料相談する'
+      #新規登録ページへ遷移している
+      expect(current_path).to eq new_user_registration_path
+      #ユーザー情報を入力する
+      fill_in 'ユーザーネーム', with: ""
+      fill_in 'メールアドレス', with: ""
+      select '---', from: "result"
+      fill_in 'パスワード', with: ""
+      fill_in 'パスワード(確認)', with: ""
+      #会員登録ボタンを押してもユーザーモデルカウントは上がらない
+      expect{find('input[name="commit"]').click}.to change {User.count}.by(0)
+      #新規登録ページに戻されている
+      expect(current_path).to eq "/users"
+    end
+  end
 end
+
