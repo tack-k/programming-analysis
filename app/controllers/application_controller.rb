49dd87923b3e7e09ad4915a05d9cf0a51a272364
tack-k <tyool9c@gmail.com>
2020-12-01 16:42:28 +0900

    ゲストユーザー作成・ログイン

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  def new_guest
+    user = User.find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+    end
+    sign_in_user
+    require_to root_path, notice: 'ゲストユーザーとしてログインしました'
+  end
 end
