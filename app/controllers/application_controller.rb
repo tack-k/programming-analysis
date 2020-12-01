da88eb2fa49f0eb7e3c1e328b0d40896387d5063
tack-k <tyool9c@gmail.com>
2020-12-01 20:21:41 +0900

    ゲストログイン後のリダイレクト先の設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,2 @@
 class ApplicationController < ActionController::Base
-  def new_guest
-    user = User.find_or_create_by!(email: 'guest@example.com') do |user|
-      user.password = SecureRandom.urlsafe_base64
-    end
-    sign_in_user
-    require_to root_path, notice: 'ゲストユーザーとしてログインしました'
-  end
 end
